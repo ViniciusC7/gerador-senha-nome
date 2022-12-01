75e81874c376f1dff506176a808751d69d68efa4
Vinicius Costa <116663758+ViniciusC7@users.noreply.github.com>
2022-12-01 00:29:54 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano, idade;
+            string nome;
+            DateTime hoje = DateTime.Today;
+            
+
+            Console.WriteLine("Seja bem-vindo, hoje é dia: " + hoje.Day + "/" + hoje.Month  + "/" + hoje.Year +" (" + hoje.DayOfWeek  + ")" );
+            Console.WriteLine("Qual é o seu nome? ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Bem-vindo, " + nome);
+            Console.WriteLine("Informe o que ano em que você nasceu ");
+            ano = int.Parse(Console.ReadLine());
+
+            idade = hoje.Year - ano;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine(nome + ", sua senha será: " + "@"+idade + nome );
+            }
+            else
+            {
+                Console.WriteLine(nome + ", sua senha será: " + "@"+nome + idade+"$" );
+            }
+
+        }
+    }
+}
